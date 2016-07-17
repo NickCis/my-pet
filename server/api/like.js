e377a9ad82b3e07ecb58c85b621e31f466215a5f
Mateo Bosco <mateo.bosco@gmail.com>
2016-07-17 00:36:12 -0300

    cuando haces un like te devuelve si tuviste un match o no

--- server/api/like.js
+++ server/api/like.js
@@ -10,10 +10,20 @@ export function post(req, res, next) {
     }
   }));
 
-  // TODO: usar scape (mirar libreria de pg para eso)
   const sql = `INSERT into likes (pet1,pet2, result) VALUES ('${req.params.pet1}', '${req.params.pet2}', '${req.params.result}')`;
   req.db.doQuery(sql)
-    .then(() => res.json(200, {success: true}))
+    .then(result => {
+      if (req.params.result != 1){ res.json(200, {success : true , match : false});}
+      else{
+        const sql2 = `SELECT result FROM likes WHERE pet1 = '${req.params.pet2}' AND pet2 = ${req.params.pet1}`;
+        req.db.doQuery(sql2)
+          .then(res2 => {
+            if (res2.rows.length == 0) { res.json(200, {success : true , match : false});}
+            if (res2.rows[0].result == 1) { res.json(200, {success : true , match : true});}
+            else {res2.json(200, {success : true , match : false});}
+          })
+        }
+    })
     .catch(err => res.json(500, {error: err}))
     .then(() => next());
 }
