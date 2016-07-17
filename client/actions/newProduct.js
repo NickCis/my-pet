8dbba22f1486089e2c9813c9ef2408901817accf
rburdet <rburdet@new-host.home>
2016-07-17 17:19:00 -0300

    Publicar producto manda info pero sin token

--- client/actions/newProduct.js
+++ client/actions/newProduct.js
@@ -4,12 +4,43 @@ import { changePageIfNeeded } from './';
 
 export const NEW_PRODUCT = 'NEW_PRODUCT';
 
-function onNewProduct(productName, productType, productPrice, productDescription) {
-  return {
-    type: NEW_PRODUCT,
-	productName,
-	productType,
-	productPrice,
-	productDescription
+
+
+export function doNewProduct(productName, productType, productPrice, productDescription) {
+  return (dispatch, getState) => {
+    return fetch('/api/product', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        productName,
+        productType,
+		productPrice,
+		productDescription
+      })
+    })
+      .then(response => response.json())
+      //.then(checkLogin)
+      //.then(token => dispatch(finishedLogin(token)))
+      .then(() => {
+        let prevPage = getState().page.previous;
+        if(['Register', 'Login'].indexOf(prevPage) != -1)
+          prevPage = 'Home';
+        return dispatch(changePageIfNeeded(prevPage));
+      })
+      .catch(error => dispatch(errorLogin(error)))
   };
 }
+
+
+/*
+	  return {
+		  type: NEW_PRODUCT,
+		  productName,
+		  productType,
+		  productPrice,
+		  productDescription
+	  };
+*/
